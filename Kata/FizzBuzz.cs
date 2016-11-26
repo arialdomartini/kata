a8a164f37a6b092e4477f44d1e3f7f75b588196a
Arialdo Martini <arialdomartini@gmail.com>
2016-11-26 07:52:01 +0100

    FizzBuzz

---
+++ Kata/FizzBuzz.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata
+{
+    public class FizzBuzz
+    {
+        public string Play()
+        {
+            var outs = new List<string>();
+            for (int i = 0; i < 100; i++)
+            {
+                var v = i + 1;
+                if (v % 3 == 0 && v % 5 == 0)  outs.Add("FizzBuzz");
+                else if (v % 3 == 0)  outs.Add("Fizz");
+                else if (v % 5 == 0)  outs.Add("Buzz");
+                else outs.Add(v.ToString());
+            }
+            return outs.Aggregate((i, j) => i + "\r\n" + j);
+        }
+    }
+}
