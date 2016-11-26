dcd8ca8cb12a89be3f91be7e63486fdd42468336
Arialdo Martini <arialdomartini@gmail.com>
2016-11-26 23:01:23 +0100

    FizzBuzz, Linq can become pretty convoluted

--- Kata/FizzBuzz.cs
+++ Kata/FizzBuzz.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
+using static System.String;
 
 namespace Kata
 {
@@ -8,17 +8,21 @@ namespace Kata
     {
         public IEnumerable<char> Play()
         {
-            var numbers = Enumerable.Range(1, 100).ToList();
+            var numbers = Enumerable.Range(1, 100);
+            return Play(numbers);
+        }
+
+        private IEnumerable<char> Play(IEnumerable<int> numbers)
+        {
             var withFizzesAndBuzzes = numbers.Select(ChangeIfNeeded);
-            return String.Join("\r\n", withFizzesAndBuzzes);
+            return Join("\r\n", withFizzesAndBuzzes);
         }
 
         private string ChangeIfNeeded(int i)
         {
-            if (i%5 == 0 && i%3 == 0) return "FizzBuzz";
-            if (i%3 == 0) return "Fizz";
-            if (i%5 == 0) return "Buzz";
-            return i.ToString();
+            var maps = new Dictionary<int, string>() { {3, "Fizz"}, {5, "Buzz"} };
+            if (maps.All(k => i%k.Key != 0)) return i.ToString();
+            return Join("", maps.Where(k => i % k.Key== 0).Select(k => k.Value));
         }
     }
 }
