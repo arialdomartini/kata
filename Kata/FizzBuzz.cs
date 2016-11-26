57ba4b62b8516c4126bf3b5d2c87ce534c0787eb
Arialdo Martini <arialdomartini@gmail.com>
2016-11-26 23:03:47 +0100

    Rules moved to the entry point

--- Kata/FizzBuzz.cs
+++ Kata/FizzBuzz.cs
@@ -8,21 +8,22 @@ namespace Kata
     {
         public IEnumerable<char> Play()
         {
+            var rules = new Dictionary<int, string>() { {3, "Fizz"}, {5, "Buzz"} };
+
             var numbers = Enumerable.Range(1, 100);
-            return Play(numbers);
+            return Play(numbers, rules);
         }
 
-        private IEnumerable<char> Play(IEnumerable<int> numbers)
+        private IEnumerable<char> Play(IEnumerable<int> numbers, Dictionary<int, string> rules)
         {
-            var withFizzesAndBuzzes = numbers.Select(ChangeIfNeeded);
+            var withFizzesAndBuzzes = numbers.Select(i => ChangeIfNeeded(i, rules));
             return Join("\r\n", withFizzesAndBuzzes);
         }
 
-        private string ChangeIfNeeded(int i)
+        private string ChangeIfNeeded(int i, Dictionary<int, string> rules)
         {
-            var maps = new Dictionary<int, string>() { {3, "Fizz"}, {5, "Buzz"} };
-            if (maps.All(k => i%k.Key != 0)) return i.ToString();
-            return Join("", maps.Where(k => i % k.Key== 0).Select(k => k.Value));
+            if (rules.All(k => i%k.Key != 0)) return i.ToString();
+            return Join("", rules.Where(k => i % k.Key== 0).Select(k => k.Value));
         }
     }
 }
