a75858986cbd01df4665a3bdb7d318e3b14affdc
Arialdo Martini <arialdomartini@gmail.com>
2016-11-27 07:44:38 +0100

    Another FizzBuzz

--- Kata/FizzBuzz.cs
+++ Kata/FizzBuzz.cs
@@ -1,10 +1,22 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 
 namespace Kata
 {
     public class FizzBuzz
     {
+        private readonly List<Matcher> _matchers;
+
+        public FizzBuzz()
+        {
+            _matchers = new List<Matcher>
+            {
+                new DividesPer(3, "Fizz"),
+                new DividesPer(5, "Buzz")
+            };
+        }
+
         public string Play()
         {
             return Play(100);
@@ -24,17 +36,44 @@ namespace Kata
 
         public string Append(int i)
         {
-            var couples = new Dictionary<int, string>();
-            couples.Add(3, "Fizz");
-            couples.Add(5, "Buzz");
+            var s = "";
+            foreach (var matcher in _matchers)
+            {
+                if (matcher.Matches(i))
+                    s += matcher.Contribute();
+            }
 
-
-            var matches = couples.Where(k => i % k.Key == 0);
-            var s = matches.Aggregate("", (current, match) => current + match.Value);
-
-            if (matches.Count() ==  0)
+            if (_matchers.Count(m=> m.Matches(i)) ==  0)
                 s = i.ToString();
             return s;
         }
+    }
+
+    public class DividesPer : Matcher
+    {
+        private readonly int _i;
+        private readonly string _str;
+
+        public DividesPer(int i, string str)
+        {
+            _i = i;
+            _str = str;
+        }
+
+        public bool Matches(int i)
+        {
+            return i%_i == 0;
+        }
+
+        public string Contribute()
+        {
+            return _str;
+        }
+    }
+
+    public interface Matcher
+    {
+        bool Matches(int i);
+        string Contribute();
     }
 }
---
+++ Kata/FizzBuzz3.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata
+{
+    public class FizzBuzz3
+    {
+        public string Play()
+        {
+            return Play(100);
+        }
+
+        public string Play(int count)
+        {
+            var s = "";
+            foreach (var i in Enumerable.Range(1, count))
+            {
+                s += Append(i);
+                if (i < 100)
+                    s += "\r\n";
+            }
+            return s;
+        }
+
+        public string Append(int i)
+        {
+            var couples = new Dictionary<int, string>();
+            couples.Add(3, "Fizz");
+            couples.Add(5, "Buzz");
+
+
+            var matches = couples.Where(k => i % k.Key == 0);
+            var s = matches.Aggregate("", (current, match) => current + match.Value);
+
+            if (matches.Count() ==  0)
+                s = i.ToString();
+            return s;
+        }
+    }
+}
