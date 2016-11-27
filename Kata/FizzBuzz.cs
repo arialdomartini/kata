a7091010c5689eea749a1b26e669e70650e7465d
Arialdo Martini <arialdomartini@gmail.com>
2016-11-27 07:46:00 +0100

    Fizz Buzz using Linq and a preloaded list of numbers

--- Kata/FizzBuzz.cs
+++ Kata/FizzBuzz.cs
@@ -1,72 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using System.Security.AccessControl;
 
 namespace Kata
 {
     public class FizzBuzz
     {
-        private readonly List<Matcher> _matchers;
-
-        public FizzBuzz()
+        public IEnumerable<char> Play()
         {
-            _matchers = new List<Matcher>
-            {
-                new DividesPer(3, "Fizz"),
-                new DividesPer(5, "Buzz")
-            };
+            var numbers = Enumerable.Range(1, 100).ToList().Select(i => i.ToString());
+            var withFizzesAndBuzzes = numbers.Select(ChangeIfNeeded);
+            return String.Join("\r\n", withFizzesAndBuzzes);
         }
 
-        public string Play()
+        private string ChangeIfNeeded(string s)
         {
-            return Play(100);
-        }
-
-        public string Play(int count)
-        {
-            var s = "";
-            foreach (var i in Enumerable.Range(1, count))
-            {
-                s += Append(i);
-                if (i < 100)
-                    s += "\r\n";
-            }
+            var s1 = Int32.Parse(s);
+            if (s1%5 == 0 && s1%3 == 0) return "FizzBuzz";
+            if (s1%3 == 0) return "Fizz";
+            if (s1%5 == 0) return "Buzz";
             return s;
         }
-
-        public string Append(int i)
-        {
-            var s = "";
-            foreach (var matcher in _matchers)
-            {
-                s += matcher.Contribute(i);
-            }
-
-            if (s == "")
-                s = i.ToString();
-            return s;
-        }
-    }
-
-    public class DividesPer : Matcher
-    {
-        private readonly int _i;
-        private readonly string _str;
-
-        public DividesPer(int i, string str)
-        {
-            _i = i;
-            _str = str;
-        }
-
-        public string Contribute(int i)
-        {
-            return i%_i == 0 ? _str : "";
-        }
-    }
-
-    public interface Matcher
-    {
-        string Contribute(int i);
     }
 }
---
+++ Kata/FizzBuzz4.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+
+namespace Kata
+{
+    public class FizzBuzz4
+    {
+        private readonly List<Matcher> _matchers;
+
+        public FizzBuzz4()
+        {
+            _matchers = new List<Matcher>
+            {
+                new DividesPer(3, "Fizz"),
+                new DividesPer(5, "Buzz")
+            };
+        }
+
+        public string Play()
+        {
+            return Play(100);
+        }
+
+        public string Play(int count)
+        {
+            var s = "";
+            foreach (var i in Enumerable.Range(1, count))
+            {
+                s += Append(i);
+                if (i < 100)
+                    s += "\r\n";
+            }
+            return s;
+        }
+
+        public string Append(int i)
+        {
+            var s = "";
+            foreach (var matcher in _matchers)
+            {
+                s += matcher.Contribute(i);
+            }
+
+            if (s == "")
+                s = i.ToString();
+            return s;
+        }
+    }
+
+    public class DividesPer : Matcher
+    {
+        private readonly int _i;
+        private readonly string _str;
+
+        public DividesPer(int i, string str)
+        {
+            _i = i;
+            _str = str;
+        }
+
+        public string Contribute(int i)
+        {
+            return i%_i == 0 ? _str : "";
+        }
+    }
+
+    public interface Matcher
+    {
+        string Contribute(int i);
+    }
+}
