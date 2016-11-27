ed6105081222d87bf513036262345f59821a13ed
Arialdo Martini <arialdomartini@gmail.com>
2016-11-27 08:06:06 +0100

    FizzBuzz

--- Kata/FizzBuzz.cs
+++ Kata/FizzBuzz.cs
@@ -1,29 +1,34 @@
-﻿using System.Collections.Generic;
 using System.Linq;
-using static System.String;
 
 namespace Kata
 {
     public class FizzBuzz
     {
-        public IEnumerable<char> Play()
+        public string Play()
         {
-            var rules = new Dictionary<int, string>() { {3, "Fizz"}, {5, "Buzz"} };
+            string s = "";
 
-            var numbers = Enumerable.Range(1, 100);
-            return Play(numbers, rules);
+            foreach (var i in Enumerable.Range(1, 100))
+            {
+                if (DivisibilePer(i, 15)) s += "FizzBuzz";
+                else if (DivisibilePer(i, 3)) s += "Fizz";
+                else if (DivisibilePer(i, 5)) s += "Buzz";
+                else s += i;
+
+                if (i!= 100)
+                    s = Separate(s);
+            }
+            return s;
         }
 
-        private IEnumerable<char> Play(IEnumerable<int> numbers, Dictionary<int, string> rules)
+        private static string Separate(string str)
         {
-            var withFizzesAndBuzzes = numbers.Select(i => ChangeIfNeeded(i, rules));
-            return Join("\r\n", withFizzesAndBuzzes);
+            return str + "\r\n";
         }
 
-        private string ChangeIfNeeded(int i, Dictionary<int, string> rules)
+        private bool DivisibilePer(int i, int n)
         {
-            if (rules.All(k => i%k.Key != 0)) return i.ToString();
-            return Join("", rules.Where(k => i % k.Key== 0).Select(k => k.Value));
+            return i % n == 0;
         }
     }
 }
---
+++ Kata/FizzBuzz5.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using static System.String;
+
+namespace Kata
+{
+    public class FizzBuzz5
+    {
+        public IEnumerable<char> Play()
+        {
+            var rules = new Dictionary<int, string>() { {3, "Fizz"}, {5, "Buzz"} };
+
+            var numbers = Enumerable.Range(1, 100);
+            return Play(numbers, rules);
+        }
+
+        private IEnumerable<char> Play(IEnumerable<int> numbers, Dictionary<int, string> rules)
+        {
+            var withFizzesAndBuzzes = numbers.Select(i => ChangeIfNeeded(i, rules));
+            return Join("\r\n", withFizzesAndBuzzes);
+        }
+
+        private string ChangeIfNeeded(int i, Dictionary<int, string> rules)
+        {
+            if (rules.All(k => i%k.Key != 0)) return i.ToString();
+            return Join("", rules.Where(k => i % k.Key== 0).Select(k => k.Value));
+        }
+    }
+}
