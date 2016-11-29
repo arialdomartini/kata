ad05559be226ca0a4b0dc20b75be4ab5cac41e82
Arialdo Martini <arialdomartini@gmail.com>
2016-11-29 21:11:32 +0100

    FizzBuzz refactored

--- Kata/FizzBuzz.cs
+++ Kata/FizzBuzz.cs
@@ -6,34 +6,39 @@ namespace Kata
 {
     public class FizzBuzz
     {
+        private readonly Dictionary<int, string> _rules;
+
+        public FizzBuzz()
+        {
+            _rules = new Dictionary<int, string>()
+            {
+                { 3, "Fizz"},
+                { 5, "Buzz"}
+            };
+        }
+
         public string Play()
         {
-            var ss = new List<string>();
+            return string.Join("\r\n", Play(100));
+        }
 
-            foreach (var i in Enumerable.Range(1, 100))
-            {
-                ss.Add(Next(i));
-            }
-            return string.Join("\r\n", ss);
+        private IEnumerable<string> Play(int count)
+        {
+            return Enumerable.Range(1, count).Select(Next);
         }
 
         private string Next(int i)
         {
-            var rules = new Dictionary<int, string>(){ { 3, "Fizz"}, {5, "Buzz"}};
-            var matching = rules.Where(w => IsMultipleOf(i, w.Key));
+            var matching = _rules.Where(w => IsMultipleOf(i, w.Key));
             if (!matching.Any())
                 return i.ToString();
+
             return string.Join("", matching.Select(k => k.Value));
         }
 
         private static bool IsMultipleOf(int i, int n)
         {
             return i % n == 0;
-        }
-
-        private static string Append(string s, string fizz)
-        {
-            return s + fizz;
         }
     }
 }
