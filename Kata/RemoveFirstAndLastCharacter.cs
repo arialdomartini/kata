6faa399e70b466b7909d18322884843a055f40cd
Arialdo Martini <arialdomartini@gmail.com>
2016-12-10 08:48:20 +0100

    Remove first and last character

---
+++ Kata/RemoveFirstAndLastCharacter.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Kata
+{
+    public class RemoveFirstAndLastCharacter
+    {
+        public static string Remove_char(string s)
+        {
+            return new string(s.ToArray().Take(s.Length -1).Skip(1).ToArray());
+        }
+    }
+
+    public class RemoveFirstAndLastCharacterTest
+    {
+        [Theory]
+        [InlineData("eloquent", "loquen")]
+        [InlineData("country", "ountr")]
+        [InlineData("person", "erso")]
+        [InlineData("place", "lac")]
+        [InlineData("ok", "")]
+        public void Test1(string given, string expected)
+        {
+            Assert.Equal(expected, RemoveFirstAndLastCharacter.Remove_char(given));
+        }
+    }
+}
