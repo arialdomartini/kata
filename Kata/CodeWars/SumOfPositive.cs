b2536d81a327aa33732501da69d71b4143add06f
Arialdo Martini <arialdomartini@gmail.com>
2016-12-10 08:23:38 +0100

    Sum of positive

---
+++ Kata/CodeWars/SumOfPositive.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Kata.CodeWars
+{
+    public class SumOfPositive
+    {
+        public static int PositiveSum(int[] arr)
+        {
+            return arr.Where(a => a > 0).Aggregate(0, (current, a) => current + a);
+        }
+    }
+
+    public class SumOfPositiveTest
+    {
+        [Theory]
+        [InlineData(new int[]{1, 2, 3, 4, 5}, 15)]
+        [InlineData(new int[]{1, -2, 3, 4, 5}, 13)]
+        [InlineData(new int[]{-1, 2, 3, 4, -5}, 9)]
+        [InlineData(new int[]{}, 0)]
+        [InlineData(new int[]{-1, -2, -3, -4, -5}, 0)]
+        public void ExampleTest(int[] arr, int result)
+        {
+            var actual = Kata.CodeWars.SumOfPositive.PositiveSum(arr);
+
+            Assert.Equal(result, actual);
+        }
+    }
+}
