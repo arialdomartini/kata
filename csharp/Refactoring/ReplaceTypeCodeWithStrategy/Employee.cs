40446a7438acb45fecb3876866200a4ab5e6e36a
Arialdo Martini <arialdomartini@gmail.com>
2016-12-20 21:53:24 +0100

    A legacy code snippet with Type Code to refactor with State Strategy
    Using the example on [Refactoring Guru](https://refactoring.guru/replace-type-code-with-state-strategy)

---
+++ csharp/Refactoring/ReplaceTypeCodeWithStrategy/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Kata.Refactoring.ReplaceTypeCodeWithStrategy
+{
+    public class Employee
+    {
+        public const int
+            Engineer = 0,
+            Salesman = 1,
+            Manager = 2;
+
+        public int Type;
+        public int MonthlySalary { get; set; }
+        public int Commission { get; set; }
+        public int Bonus { get; set; }
+
+        public Employee(int type)
+        {
+            Type = type;
+        }
+
+        public int PayAmount()
+        {
+            switch (Type)
+            {
+                case Engineer:
+                    return MonthlySalary;
+                case Salesman:
+                    return MonthlySalary + Commission;
+                case Manager:
+                    return MonthlySalary + Bonus;
+                default:
+                    throw new Exception("Incorrect Employee Type");
+            }
+        }
+    }
+}
