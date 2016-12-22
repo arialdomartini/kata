6cc28a798bd1b72295af48908995c86e72b2f292
Arialdo Martini <arialdomartini@gmail.com>
2016-12-22 22:24:28 +0100

    Morse Code decoder kata

---
+++ csharp/CodeWars/MorseCodeKata/MorseCodeDecoder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Kata.CodeWars.MorseCodeKata
+{
+    class MorseCodeDecoder
+    {
+        public static string Decode(string morseCode)
+        {
+            return string.Join(" ",
+                morseCode.Split(new[] {"  "}, StringSplitOptions.None)
+                    .Select(word => string.Join("",
+                        word.Split(' ').Select(MorseCode.Get)
+                    )));
+        }
+    }
+
+    internal class MorseCode
+    {
+        public static string Get(string value)
+        {
+            return "";
+        }
+    }
+
+
+    public class MorseCodeDecoderTests
+    {
+        [Fact]
+        public void MorseCodeDecoderBasicTest_1()
+        {
+            var input = ".... . -.--   .--- ..- -.. .";
+            var expected = "HEY JUDE";
+
+            var actual = MorseCodeDecoder.Decode(input);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
