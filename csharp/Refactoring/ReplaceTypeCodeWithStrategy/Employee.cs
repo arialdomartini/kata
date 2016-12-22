ddb0609ee87898685c68611080949ff1468c6f1b
Arialdo Martini <arialdomartini@gmail.com>
2016-12-22 08:52:08 +0100

    Employee legacy code refactored with State/Strategy

--- csharp/Refactoring/ReplaceTypeCodeWithStrategy/Employee.cs
+++ csharp/Refactoring/ReplaceTypeCodeWithStrategy/Employee.cs
@@ -1,37 +1,90 @@
 ﻿using System;
+using System.Text;
 
 namespace Kata.Refactoring.ReplaceTypeCodeWithStrategy
 {
     public class Employee
+    {
+        private EmployeeType _employeeType;
+
+        public int EmployeeCode
+        {
+            get { return _employeeType.EmployeeCode; }
+            set { _employeeType = EmployeeType.Create(value); }
+        }
+
+        public int MonthlySalary { get; set; }
+        public int Commission { get; set; }
+        public int Bonus { get; set; }
+
+        public Employee(int typeCode)
+        {
+            EmployeeCode = typeCode;
+        }
+
+        public int PayAmount()
+        {
+            return _employeeType.PayAmount(this);
+        }
+    }
+
+
+    abstract class EmployeeType
     {
         public const int
             Engineer = 0,
             Salesman = 1,
             Manager = 2;
 
-        public int Type;
-        public int MonthlySalary { get; set; }
-        public int Commission { get; set; }
-        public int Bonus { get; set; }
-
-        public Employee(int type)
+        public static EmployeeType Create(int code)
         {
-            Type = type;
-        }
-
-        public int PayAmount()
-        {
-            switch (Type)
+            switch (code)
             {
                 case Engineer:
-                    return MonthlySalary;
+                    return new Engineer();
+
                 case Salesman:
-                    return MonthlySalary + Commission;
+                    return new Salesman();
+
                 case Manager:
-                    return MonthlySalary + Bonus;
+                    return new Manager();
+
                 default:
                     throw new Exception("Incorrect Employee Type");
             }
+        }
+
+        public abstract int EmployeeCode { get; }
+
+        public abstract int PayAmount(Employee employee);
+
+
+    }
+
+    class Salesman : EmployeeType
+    {
+        public override int EmployeeCode => EmployeeType.Salesman;
+        public override int PayAmount(Employee employee)
+        {
+            return employee.MonthlySalary + employee.Commission;
+        }
+    }
+
+    class Manager : EmployeeType
+    {
+        public override int EmployeeCode => EmployeeType.Manager;
+        public override int PayAmount(Employee employee)
+        {
+            return employee.MonthlySalary + employee.Bonus;
+        }
+    }
+
+    class Engineer : EmployeeType
+    {
+        public override int EmployeeCode => EmployeeType.Engineer;
+        public override int PayAmount(Employee employee)
+        {
+            return employee.MonthlySalary;
         }
     }
 }
